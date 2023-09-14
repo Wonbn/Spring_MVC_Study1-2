ec912a5f9aad908271e2c866c3b59022b05a2c3b
rmaWhrdl <wwscan3@gmail.com>
2023-09-14 23:53:14 +0900

    <스프링 MVC - 기본 기능> 요청 매핑
    - @RestController
       * @Controller 는 반환 값이 String 이면 뷰 이름으로 인식된다. 그래서 뷰를 찾고 뷰가 랜더링 된다.
       * @RestController 는 반환 값으로 뷰를 찾는 것이 아니라, HTTP 메시지 바디에 바로 입력한다.
       * 따라서 실행 결과로 ok 메세지를 받을 수 있다.
    
    - @RequestMapping("/hello-basic")
       * /hello-basic URL 호출이 오면 이 메서드가 실행되도록 매핑한다.
       * 대부분의 속성을 배열[] 로 제공하므로 다중 설정이 가능하다. {"/hello-basic", "/hello-go"}
    
    - 스프링 부트 3.0 이후
       * 스프링 부트 3.0 부터는 /hello-basic , /hello-basic/ 는 서로 다른 URL 요청을 사용해야 한다.
       * 기존에는 마지막에 있는 / (slash)를 제거했지만, 스프링 부트 3.0 부터는 마지막의 / (slash)를 유지한다.
       * 따라서 다음과 같이 다르게 매핑해서 사용해야 한다.
       * 매핑: /hello-basic   ->  URL 요청: /hello-basic
       * 매핑: /hello-basic/  ->  URL 요청: /hello-basic/
    
    - PathVariable(경로 변수) 사용   * 경로에 변수를 넣어서 사용한다.
       * 그 변수를 사용하려면 @PathVariable("userId") String data 이런 식으로 불러올 수 있다.
    
    - 특정 헤더 조건 매핑
    - 미디어 타입 조건 매핑 [ HTTP 요청 Content-Type, consumes ]
       * consumes 을 지정함으로써 경로와 Content-Type 까지 맞아야 매핑할 수 있게 한다.
    - 미디어 타입 조건 매핑 [ HTTP 요청 Accept, produces ]
       * produces 을 지정함으로써 경로와 Accept 까지 맞아야 매핑할 수 있게 한다.
    
    - Accept vs Content-Type
       * 헤더 정보 중 Accept 타입에 대한 설명은 아래와 같다.
       * "클라이언트가 서버에게 요청을 하는데.. 너가 나에게 응답을 줄때는 Accept 에 저장된 형식에 맞게 보내줘."
    
       * 헤더 정보 중 Content-Type 타입에 대한 설명은 아래와 같다.
       * "클라이언트가 서버에게 요청을 하는데.. 내가 보내는 요청의 데이터 형식은 Content-Type 에 저장된 형식이야"

---
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -0,0 +1,127 @@
+package hello.springmvc.basic.requestmapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MappingController {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    /**
+     * 기본 요청
+     * /hello-basic, /hello-basic/ 둘다 허용
+     * HTTP 메서드 모두 허용 GET, HEAD, POST, PUT, PATCH, DELETE
+     */
+    @RequestMapping(value = {"/hello-basic", "/hello-go"})
+    public String helloBasic() {
+        log.info("helloBasic");
+        return "ok";
+    }
+
+    /**
+     * method 값을 이용해서 특정 HTTP 메서드 요청만 허용
+     * GET, HEAD, POST, PUT, PATCH, DELETE
+     */
+    @RequestMapping(value = "/mapping-get-v1", method = RequestMethod.GET)
+    public String mappingGetV1() {
+        log.info("mappingGetV1");
+        return "ok";
+    }
+
+    /**
+     * 편리한 축약 애노테이션
+     * @GetMapping
+     * @PostMapping
+     * @PutMapping
+     * @DeleteMapping
+     * @PatchMapping
+     */
+    @GetMapping(value = "/mapping-get-v2")
+    public String mappingGetV2() {
+        log.info("mapping-get-v2");
+        return "ok";
+    }
+
+    /**
+     * PathVariable 형식을 요즘 많이 사용함.
+     * PathVariable 사용
+     * [ 변수명이 같으면 생략 가능 ]
+     * @PathVariable("userId") String userId -> @PathVariable userId
+     */
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data) {
+        log.info("mappingPath user-Id={}", data);
+        return "ok";
+    }
+
+    /**
+     * PathVariable 형식을 요즘 많이 사용함.
+     * PathVariable 사용 다중
+     */
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    public String mappingPath(@PathVariable String userId, @PathVariable Long orderId) {
+        log.info("mappingPath userId={}, orderId={}", userId, orderId);
+        return "ok";
+    }
+
+    /**
+     * 파라미터로 추가 매핑 
+     * (경로뿐만 아니라 params 로 지정한 파라미터까지 있어야 매핑 가능)
+     * params="mode",
+     * params="!mode"
+     * params="mode=debug"
+     * params="mode!=debug" (! = )
+     * params = {"mode=debug","data=good"}
+     */
+    @GetMapping(value = "/mapping-param", params = "mode=debug")
+    public String mappingParam() {
+        log.info("mappingParam");
+        return "ok";
+    }
+
+    /**
+     * 특정 헤더로 추가 매핑
+     * (경로뿐만 아니라 headers 로 지정한 헤더까지 있어야 매핑 가능)
+     * headers="mode",
+     * headers="!mode"
+     * headers="mode=debug"
+     * headers="mode!=debug" (! = )
+     */
+    @GetMapping(value = "/mapping-header", headers = "mode=debug")
+    public String mappingHeader() {
+        log.info("mappingHeader");
+        return "ok";
+    }
+
+    /**
+     * Content-Type 헤더 기반 추가 매핑 Media Type
+     * (경로뿐만 아니라 consumes 로 헤더의 Content-Type 이 application/json 이어야 매핑 가능)
+     * consumes="application/json"
+     * consumes="!application/json"
+     * consumes="application/*"
+     * consumes="*\/*"
+     * MediaType.APPLICATION_JSON_VALUE
+     */
+    @PostMapping(value = "/mapping-consume", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String mappingConsumes() {
+        log.info("mappingConsumes");
+        return "ok";
+    }
+
+    /**
+     * Accept 헤더 기반 Media Type
+     * (경로뿐만 아니라 produces 로 헤더의 Accept 가 text/html 이어야 매핑 가능)
+     * produces = "text/html"
+     * produces = "!text/html"
+     * produces = "text/*"
+     * produces = "*\/*"
+     */
+    @PostMapping(value = "/mapping-produce", produces = MediaType.TEXT_HTML_VALUE)
+    public String mappingProduces() {
+        log.info("mappingProduces");
+        return "ok";
+    }
+}
